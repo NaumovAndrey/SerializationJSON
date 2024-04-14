436ecfe2373a19f1349e62151cbaf100023a4a4d
andrey <naumov.a.n@icloud.com>
2024-04-14 22:16:00 +0700

    Добавьте файлы проекта.

---
+++ SerializationJSON.cpp
@@ -0,0 +1,12 @@
+﻿// SerializationJSON.cpp: определяет точку входа для приложения.
+//
+
+#include "SerializationJSON.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
