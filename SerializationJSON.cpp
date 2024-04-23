ad6c30ef392e540d60666d63185c8d4f07a2fd15
andrey <naumov.a.n@icloud.com>
2024-04-23 22:42:38 +0700

    lesson part3 read and write file

--- SerializationJSON.cpp
+++ SerializationJSON.cpp
@@ -5,21 +5,54 @@
 #include <iostream>
 #include "string"
 #include "fstream"
+#include "nlohmann/json.hpp"
 
 using namespace std;
 
-struct Record{
+struct Record {
     string name;
     string famaly;
     int age;
+    bool married;
 };
 
-int main()
-{
+int main() {
     Record record;
-    cin >> record.name >> record.famaly;
-    ofstream file("record.json");
-    //file << "{" << "name: "<< record.name<< ", famaly: "<< record.famaly << "}" << endl;
+//    cin >> record.name >> record.famaly >> record.age >> record.married;
+//    ofstream file("record.json");
 
-	return 0;
+
+//Записать в файл
+
+    //способ 1
+
+//    nlohmann::json dict;
+//    dict["name"] = record.name;
+//    dict["family"] = record.famaly;
+//    dict["age"] = record.age;
+//    dict["married"] = record.married;
+
+    //способ 2
+
+//    nlohmann::json dict = {
+//            {"name", record.name},
+//            {"family", record.famaly},
+//            {"age", record.age},
+//            {"married", record.married}
+//    };
+
+//    file << dict;
+
+//Прочитать из файла
+
+    ifstream file("record.json");
+    nlohmann::json dict;
+    file >> dict;
+    record.name = dict["name"];
+    record.famaly = dict["family"];
+    record.age = dict["age"];
+    record.married = dict["married"];
+    cout << record.name << " " << record.famaly << " " << record.age << " " << record.married << endl;
+
+    return 0;
 }
